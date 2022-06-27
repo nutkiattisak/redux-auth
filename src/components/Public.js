ad17e9b58f2a023097c97d410faca71d1831bef9
kiattisak <kiattisak.ch1995@gmail.com>
2022-06-27 23:32:23 +0700

    Initial commit

---
+++ src/components/Public.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Public = () => {
+  const content = (
+    <>
+      <div>Public</div>
+    </>
+  );
+  return content;
+};
+
+export default Public;
